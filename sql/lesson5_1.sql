c1312248c1f9ef4f62a2284acfc0d23b3577b5e9
roberthsu2003 <roberthsu2003@gmail.com>
2024-03-09 10:23:10 +0800

    Update lesson5_1.sql

--- sql/lesson5_1.sql
+++ sql/lesson5_1.sql
@@ -17,4 +17,13 @@ CREATE TABLE IF NOT EXISTS 聯絡人(
 	FOREIGN KEY(客戶id) REFERENCES 客戶(客戶_id)
 );
 
+INSERT INTO 客戶(客戶名稱)
+VALUES('遠傳電信'),
+      ('台灣大車隊');
+
+INSERT INTO 聯絡人(客戶id,聯絡人姓名,電話,郵件)
+VALUES (1,'John Doe','(408)-111-1234','john.doe@bluebird.dev'),
+	   (1,'Jane Doe','(408)-111-1235','jane.doe@bluebird.dev'),
+	   (2,'David Wright','(408)-222-1234','david.wright@dolphin.dev');
+
 
