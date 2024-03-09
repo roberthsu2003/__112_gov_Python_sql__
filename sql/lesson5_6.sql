89a194db27c70342fba8249db5be669d085b1c5f
roberthsu2003 <roberthsu2003@gmail.com>
2024-03-09 11:57:14 +0800

    Create lesson5_6.sql

---
+++ sql/lesson5_6.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS stations;
+CREATE TABLE IF NOT EXISTS stations(
+	編號 INT PRIMARY KEY,
+	名稱 VARCHAR(20) NOT NULL,
+	英文名稱 VARCHAR(50),
+	地名 VARCHAR(20),
+	英文地名 VARCHAR(50),
+	地址 VARCHAR(255),
+	英文地址 VARCHAR(255),
+	電話 VARCHAR(20),
+	gps VARCHAR(50),
+	youbike BOOL
+);
