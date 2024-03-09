c9ce148a7c38eeccb0646a0a87bc3587cdbe931c
roberthsu2003 <roberthsu2003@gmail.com>
2024-03-09 15:40:46 +0800

    Update lesson6_3.sql

--- sql/lesson6_3.sql
+++ sql/lesson6_3.sql
@@ -2,3 +2,10 @@
 SELECT customer_id, COUNT(*) AS 筆數
 FROM payment
 GROUP BY customer_id
+
+/*取出payment的所有客戶的的應收帳款總合,SUM()*/
+SELECT customer_id, SUM(amount) AS 總合
+FROM payment
+GROUP BY customer_id
+ORDER BY 總合 DESC;
+
