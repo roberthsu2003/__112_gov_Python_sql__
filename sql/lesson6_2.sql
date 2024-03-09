d97769dcc2d8be1fd2e65917fa0d2992f3d3c5a6
roberthsu2003 <roberthsu2003@gmail.com>
2024-03-09 14:48:23 +0800

    Update lesson6_2.sql

--- sql/lesson6_2.sql
+++ sql/lesson6_2.sql
@@ -9,3 +9,9 @@ SELECT DISTINCT 名稱
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
 WHERE 地址 LIKE '高雄市%';
 
+/*取出基隆火車站2022年3月1日資料,'2022-03-01'*/
+
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 名稱 = '基隆' AND 日期 = '2022-03-01';
+
