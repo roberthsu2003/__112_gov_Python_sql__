73a6244ff59d0e52ac0a409c25d581cdc41ecb36
roberthsu2003 <roberthsu2003@gmail.com>
2024-03-09 14:55:22 +0800

    Update lesson6_2.sql

--- sql/lesson6_2.sql
+++ sql/lesson6_2.sql
@@ -15,3 +15,10 @@ SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
 WHERE 名稱 = '基隆' AND 日期 = '2022-03-01';
 
+/*取出基隆火車站2022年3月份資料,時間由小到大排序*/
+
+SELECT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE 名稱 = '基隆' AND (日期 BETWEEN '2022-03-01' AND '2022-03-31')
+ORDER BY 日期 ASC;
+
