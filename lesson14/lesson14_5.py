109ff1f14fdd1a34753a14fd2e828c49e509a0c1
徐國堂 <roberthsu2003@gmail.com>
2024-04-13 08:08:09 +0000

    modify

--- lesson14/lesson14_5.py
+++ lesson14/lesson14_5.py
@@ -19,7 +19,25 @@ def get_contacts() -> list:
                 contacts.append({'id':item[0],'客戶名稱':item[1],'姓名':item[2],'電話':item[3],'郵件':item[4]})
             return contacts
         
+@st.cache_resource
+def get_names() -> list:
+    with psycopg2.connect(os.environ['POSTGRE_PASSWORD']) as conn:
+        with conn.cursor() as cursor:
+            sql='''
+                SELECT DISTINCT 客戶名稱
+                FROM 聯絡人 LEFT JOIN 客戶 ON 聯絡人.客戶id = 客戶.客戶_id
+            '''
+            cursor.execute(sql)
+            datas:list[tuple] = cursor.fetchmany(10)
+            names = []
+            for item in datas:
+                names.append(item[0])
+            return names
+            
 
-source_data:list[dict] = get_contacts()
 
-st.dataframe(source_data,width=1200)
+with st.sidebar:
+    option = st.selectbox("請輸入客戶",get_names())
+    st.write('You selected:', option)
+
+
