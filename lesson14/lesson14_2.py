8bb581a62bbceaa1d3d8be93375c1df4b41b58ef
徐國堂 <roberthsu2003@gmail.com>
2024-04-13 06:21:39 +0000

    modify

---
+++ lesson14/lesson14_2.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("森永動物園")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
