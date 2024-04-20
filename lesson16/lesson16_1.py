5f38722061d5762ac3c42fa8ddaf57cc8084507b
徐國堂 <roberthsu2003@gmail.com>
2024-04-20 06:26:27 +0000

    modify

--- lesson16/lesson16_1.py
+++ lesson16/lesson16_1.py
@@ -1,17 +1,26 @@
 import streamlit as st
 
 st.title("Sessin_state基礎")
-st.write(st.session_state)
+#st.write(st.session_state)
 ##使用 input widget
 number:int = st.slider("數值",min_value=1,max_value=10,value=5,key='mySlider')
-st.write("加入slider後的session_state",st.session_state)
+#st.write("加入slider後的session_state",st.session_state)
 
 col1, buff, col2= st.columns([1, 0.5, 3])
+
+next = st.button("下一個選項")
+if next:
+    if st.session_state.radio_option == "a":
+        st.session_state.radio_option = "b"
+    elif st.session_state.radio_option == "b":
+        st.session_state.radio_option = "c"
+    else:
+        st.session_state.radio_option = "a"
 
 with col1:
     option_names = ["a", "b", "c"]
     option = st.radio("請選擇1個",option_names,key="radio_option")
-    st.write("加入radio後的session_state",st.session_state)
+    #st.write("加入radio後的session_state",st.session_state)
 
 with col2:
     if option == 'a':
@@ -20,3 +29,4 @@ with col2:
         st.write("您選擇的是'b' :heart:")
     elif option == 'c':
         st.write("您選擇的是'c' :rocket:")
+
