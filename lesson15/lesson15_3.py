224bd8fb6851f203a8822eabb7058ca3b03a32ae
徐國堂 <roberthsu2003@gmail.com>
2024-04-20 03:26:22 +0000

    modify

--- lesson15/lesson15_3.py
+++ lesson15/lesson15_3.py
@@ -1,2 +1,13 @@
 import streamlit as st
 
+#建立預設的session_state的key 
+if 'a1' not in st.session_state:
+    st.session_state['a1'] = 100
+
+if 'a2' not in st.session_state:
+    st.session_state['a2'] = 200
+
+if 'a3' not in st.session_state:
+    st.session_state['a3'] = 300
+
+st.write(st.session_state)
